b404841763b5b29dcb5de1ac440d9b17cdafaa6b
Bastien Perdriau <bastien.perdriau@gmail.com>
2018-04-13 00:37:38 +0200

    Adding AssemblyCompany as Author for NuGet package

--- Sources/SharePointRunner/Properties/AssemblyInfo.cs
+++ Sources/SharePointRunner/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("SharePointRunner")]
 [assembly: AssemblyDescription("Tool to run processes accross a SharePoint Online structure, from tenant to list items and files")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Bastien Perdriau")]
 [assembly: AssemblyProduct("SharePointRunner")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
