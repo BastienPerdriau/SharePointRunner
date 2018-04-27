7188d6fdf382b73dbe8d94fd843cdf36bf55e064
Bastien PERDRIAU <bastien.perdriau@live.fr>
2018-04-27 21:06:48 +0200

    Error correction

--- Tools/SharePointRunner.Tools/Publish-Package.ps1
+++ Tools/SharePointRunner.Tools/Publish-Package.ps1
@@ -16,5 +16,5 @@ Write-Output "RepositoryName $RepositoryName"
 Write-Output "RepositoryApiKey $RepositoryApiKey"
 Write-Output "PackageName $PackageName"
 
-Publish-Module -Name PackageName -Repository $RepositoryName -NuGetApiKey $RepositoryApiKey
+Publish-Module -Name $PackageName -Repository $RepositoryName -NuGetApiKey $RepositoryApiKey
 Write-Output "Debug 1"
