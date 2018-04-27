e74c16700d8a1ed36cdb9431234966aa6ad6fd08
Bastien PERDRIAU <bastien.perdriau@live.fr>
2018-04-27 21:20:17 +0200

    Correction to Publish-Module using -Path instead of -Name

--- Tools/SharePointRunner.Tools/Publish-Package.ps1
+++ Tools/SharePointRunner.Tools/Publish-Package.ps1
@@ -16,5 +16,6 @@ Write-Output "RepositoryName $RepositoryName"
 Write-Output "RepositoryApiKey $RepositoryApiKey"
 Write-Output "PackageName $PackageName"
 Get-ChildItem -Recurse
-Publish-Module -Name $PackageName -Repository $RepositoryName -NuGetApiKey $RepositoryApiKey
+# Publish-Module -Name $PackageName -Repository $RepositoryName -NuGetApiKey $RepositoryApiKey
+Publish-Module -Path .\$PackageName -Repository $RepositoryName -NuGetApiKey $RepositoryApiKey
 Write-Output "Debug 1"
